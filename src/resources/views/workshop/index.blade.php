264778eeb3d4cb985fdfd76e31f8e1f926685567
2bo <developer.2bo@gmail.com>
2019-09-01 18:07:20 +0900

    add temporary workshops index page

---
+++ src/resources/views/workshop/index.blade.php
@@ -0,0 +1,12 @@
+<ul>
+    @foreach ($workshops as $workshop)
+        <div>
+            <a href="{{$workshop->event_url}}">{{$workshop->title}}</a>
+            <ul>
+                @foreach($workshop->tags as $tag)
+                    <li>{{$tag->name}}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endforeach
+</ul>

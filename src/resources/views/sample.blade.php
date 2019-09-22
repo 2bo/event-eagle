63e3a0bbbbed9e1b9859273bb90242416863b4ca
2bo <developer.2bo@gmail.com>
2019-09-22 11:48:46 +0900

    add bulma and make sample header component

--- src/resources/views/sample.blade.php
+++ src/resources/views/sample.blade.php
@@ -5,10 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Title</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
-<h1>sample</h1>
 <div id="app">
+    <example-header-component></example-header-component>
     <example-component></example-component>
 </div>
 

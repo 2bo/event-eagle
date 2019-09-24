b5a76c1560f28deeca62ce8a23653096b7ba5acb
2bo <developer.2bo@gmail.com>
2019-09-24 16:21:27 +0900

    modify html with bulma

--- src/resources/views/workshop/index.blade.php
+++ src/resources/views/workshop/index.blade.php
@@ -1,14 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
-    @foreach ($workshops as $workshop)
-        <div>
-            <a href="{{$workshop->event_url}}">{{$workshop->title}}</a>
-            <ul>
-                @foreach($workshop->tags as $tag)
-                    <li>{{$tag->name}}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endforeach
+    <div class="container">
+        @foreach($workshops as $workshop)
+            <article class="media">
+                <div class="media-content">
+                    <div class="content">
+                        <p>
+                            <strong>
+                                <a href="{{$workshop->event_url}}" target="_blank"
+                                   rel="noopener">{{$workshop->title}}</a>
+                            </strong>
+                            {{$workshop->started_at}}
+                            <br>
+                            {{$workshop->catch}}
+                            <br>
+                            <small>{{$workshop->address}}</small>
+                        </p>
+                    </div>
+                    <nav class="level is-mobile">
+                        <div class="level-left">
+                            @foreach($workshop->tags as $tag)
+                                <a href="" class="level-item">
+                                    <span class="tag">{{$tag->name}}</span>
+                                </a>
+                            @endforeach
+                        </div>
+                    </nav>
+                </div>
+            </article>
+        @endforeach
+    </div>
+    <div class="container">
+        {{ $workshops->links() }}
+    </div>
 @endsection
+

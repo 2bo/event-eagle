d63e2b47cef51830842d1a976f882e04ef194a78
2bo <developer.2bo@gmail.com>
2020-06-21 00:02:11 +0900

    add type id const value

--- src/app/Domain/Models/Event/EventType.php
+++ src/app/Domain/Models/Event/EventType.php
@@ -6,10 +6,20 @@ namespace App\Domain\Models\Event;
 
 class EventType
 {
+    const LT = 1;
+    const MOKUMOKU = 2;
+    const HANDS_ON = 3;
+    const HACKKATHON = 4;
+    const SEMINAR = 5;
+    const CONFERENCE = 6;
+    const WORKSHOP = 7;
+    const READING = 8;
+
     private $id;
     private $name;
     private $needle;
 
+    //FIXME $idを渡さないように修正する
     public function __construct(int $id, string $name = null, string $needle = null)
     {
         $this->id = $id;

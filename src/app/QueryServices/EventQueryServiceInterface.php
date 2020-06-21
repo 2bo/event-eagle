94e3f8edd2a718009ddd8fe5c9d8aaac54e30615
2bo <developer.2bo@gmail.com>
2020-06-21 10:37:52 +0900

    refactor code

--- src/app/QueryServices/EventQueryServiceInterface.php
+++ src/app/QueryServices/EventQueryServiceInterface.php
@@ -7,6 +7,7 @@ use DateTime;
 
 interface EventQueryServiceInterface
 {
-    public function searchEvent(?DateTime $from = null, ?DateTime $to = null, ?string $freeText = null, ?array $prefectures = null, ?array $types = null, ?bool $isOnline = null,
+    public function searchEvent(?DateTime $from = null, ?DateTime $to = null, ?string $freeText = null,
+                                ?array $prefectures = null, ?array $types = null, ?bool $isOnline = null,
                                 int $page = 1, int $perPage = 15): PaginateResult;
 }

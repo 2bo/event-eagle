c0a28926499e53d90896e6ab4677adb1ff2bf60c
2bo <developer.2bo@gmail.com>
2020-07-12 16:47:42 +0900

    add test for findById()

--- src/tests/Unit/Repositories/EventRepositoryTest.php
+++ src/tests/Unit/Repositories/EventRepositoryTest.php
@@ -2,16 +2,17 @@
 
 namespace Tests\Repositories\Unit;
 
+use App\DataModels\Event as EventDataModel;
 use App\Domain\Models\Event\ConnpassEvent;
-use App\Domain\Models\Event\Tag;
+use App\Domain\Models\Event\Event;
 use App\Domain\Models\Event\EventType;
 use App\Domain\Models\Prefecture\PrefectureId;
-use App\Domain\Models\Event\Event;
+use App\Repositories\EventRepository;
+use App\Repositories\EventTypeRepository;
+use App\Repositories\TagRepository;
+use DateTime;
 use Illuminate\Support\Facades\Artisan;
 use Tests\TestCase;
-use DateTime;
-use App\DataModels\Event as EventDataModel;
-use App\Repositories\EventRepository;
 
 class EventRepositoryTest extends TestCase
 {
@@ -60,15 +61,17 @@ class EventRepositoryTest extends TestCase
         $eventCreatedAt = new DateTime('2020-01-01 15:00:00');
         $eventUpdatedAt = new DateTime('2020-01-01 20:00:00');
         $isOnline = true;
-        //FIXME repositoryからドメインクラスを取得する
+
+        $typeRepository = new EventTypeRepository();
         $types = [
-            new EventType(1, 'name', 'needle'),
-            new EventType(2, 'name', 'needle')
+            $typeRepository->findById(EventType::MOKUMOKU),
+            $typeRepository->findById(EventType::HANDS_ON),
         ];
-        //FIXME repositoryからドメインクラスを取得する
+
+        $tagsRepository = new TagRepository();
         $tags = [
-            new Tag(1, 'name'),
-            new Tag(2, 'name')
+            $tagsRepository->findByName('PHP'),
+            $tagsRepository->findByName('Laravel'),
         ];
 
         $event = new Event(
@@ -135,11 +138,112 @@ class EventRepositoryTest extends TestCase
 
     public function testFindById()
     {
+        $siteName = ConnpassEvent::SITE_NAME_CONNPASS;
+        $eventId = 10;
+        $title = 'title';
+        $catch = 'catch';
+        $description = 'description';
+        $eventUrl = 'https://example.com';
+        $prefectureId = new PrefectureId(10);
+        $address = 'address';
+        $place = 'place';
+        $lat = 0.1;
+        $lon = 0.2;
+        $startedAt = new DateTime('2020-01-01 00:00:00');
+        $endedAt = new DateTime('2020-01-01 10:00:00');
+        $limit = 20;
+        $participants = 30;
+        $waiting = 40;
+        $owner_id = 100;
+        $owner_nickname = 'owner_nickname';
+        $owner_twitter_id = 'owner_twitter_id';
+        $owner_display_name = 'owner_display_name';
+        $group_id = 1000;
+        $eventCreatedAt = new DateTime('2020-01-01 15:00:00');
+        $eventUpdatedAt = new DateTime('2020-01-01 20:00:00');
+        $isOnline = true;
+
+        $typeRepository = new EventTypeRepository();
+        $types = [
+            $typeRepository->findById(EventType::MOKUMOKU),
+            $typeRepository->findById(EventType::HANDS_ON),
+        ];
+
+        $tagsRepository = new TagRepository();
+        $tags = [
+            $tagsRepository->findByName('PHP'),
+            $tagsRepository->findByName('Laravel'),
+        ];
+
+        $event = new Event(
+            null,
+            $siteName,
+            $eventId,
+            $title,
+            $catch,
+            $description,
+            $eventUrl,
+            $prefectureId,
+            $address,
+            $place,
+            $lat,
+            $lon,
+            $startedAt,
+            $endedAt,
+            $limit,
+            $participants,
+            $waiting,
+            $owner_id,
+            $owner_nickname,
+            $owner_twitter_id,
+            $owner_display_name,
+            $group_id,
+            $eventCreatedAt,
+            $eventUpdatedAt,
+            $isOnline,
+            $types,
+            $tags
+        );
+
         $repository = new EventRepository();
-        $id = 1;
-        $repository->updateOrCreateEvent(new Event(null, ConnpassEvent::SITE_NAME_CONNPASS, $id));
-        $event = $repository->findById($id);
+        $repository->updateOrCreateEvent($event);
+        $latestId = (\DB::Table('events')->orderBy('id', 'desc')->first())->id;
+        $event = $repository->findById($latestId);
+
         self::assertInstanceOf(Event::class, $event);
-        self::assertEquals($id, $event->getId());
+        self::assertEquals($latestId, $event->getId());
+        self::assertEquals($siteName, $event->getSiteName());
+        self::assertEquals($eventId, $event->getEventId());
+        self::assertEquals($title, $event->getTitle());
+        self::assertEquals($catch, $event->getCatch());
+        self::assertEquals($description, $event->getDescription());
+        self::assertEquals($eventUrl, $event->getEventUrl());
+        self::assertEquals($prefectureId->value(), $event->getPrefectureId()->value());
+        self::assertEquals($address, $event->getAddress());
+        self::assertEquals($place, $event->getPlace());
+        self::assertEquals($lat, $event->getLat());
+        self::assertEquals($lon, $event->getLon());
+        self::assertEquals($startedAt, $event->getStartedAt());
+        self::assertEquals($endedAt, $event->getEndedAt());
+        self::assertEquals($limit, $event->getLimit());
+        self::assertEquals($participants, $event->getParticipants());
+        self::assertEquals($waiting, $event->getWaiting());
+        self::assertEquals($owner_id, $event->getOwnerId());
+        self::assertEquals($owner_nickname, $event->getOwnerNickname());
+        self::assertEquals($owner_twitter_id, $event->getOwnerTwitterId());
+        self::assertEquals($owner_display_name, $event->getOwnerDisplayName());
+        self::assertEquals($group_id, $event->getGroupId());
+        self::assertEquals($eventCreatedAt, $event->getEventCreatedAt());
+        self::assertEquals($eventUpdatedAt, $event->getEventUpdatedAt());
+        self::assertEquals($isOnline, $event->isOnline());
+        self::assertEquals($types, $event->getTypes());
+        self::assertEquals($tags, $event->getTags());
+    }
+
+    public function testFindByIdWhenNotFound()
+    {
+        $repository = new EventRepository();
+        $event = $repository->findById(-1);
+        self::assertNull($event);
     }
 }
